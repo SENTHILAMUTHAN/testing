9b38bc3958d854f9c1f1b2275f957f32538da822
SENTHILAMUTHAN <senthilamuthan97@gmail.com>
2024-05-09 17:28:06 +0530

    new update

--- src/org/phone/ExternalStorage.java
+++ src/org/phone/ExternalStorage.java
@@ -6,6 +6,11 @@ public class ExternalStorage {
 	public void size() {
 		// TODO Auto-generated method stub
             System.out.println("External storage:\n"+"\tExpandable till 1 TB\n");
+            
+            
+            
+            int a=10;
+            System.out.println("updating");
 	}
 
 }
